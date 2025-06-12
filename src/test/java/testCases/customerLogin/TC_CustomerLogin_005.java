eacfe056e7e79989dbddad2722369c4ec2d8b90e
vivek dubey <vivek.dubey521@gmail.com>
2025-06-12 09:01:47 -0700

    Customer Login test Cases

---
+++ src/test/java/testCases/customerLogin/TC_CustomerLogin_005.java
@@ -0,0 +1,64 @@
+//Validate logging into the Application using invalid credentials (i.e. Invalid email address and Invalid Password)
+
+package testCases.customerLogin;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import pageObjects.CustomerLogin;
+import pageObjects.HomeFrontEndPage;
+
+@Listeners(utilities.TestListener.class)
+
+public class TC_CustomerLogin_005 {
+	
+WebDriver driver;
+	
+	@BeforeClass
+	public void setUp() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://www.quickvee.com/");
+	}
+	
+	@Test(priority = 1)
+	public void InvalidEmailPass() throws InterruptedException {
+		HomeFrontEndPage homePage = new HomeFrontEndPage(driver);
+		homePage.loginBtn();
+		
+		CustomerLogin customerLogin = new CustomerLogin(driver);
+		
+		customerLogin.setEmail("vivek22@lgmail.com");
+		String emailPut = customerLogin.getEmail();
+		System.out.println("Email: "+emailPut);
+		
+		customerLogin.setPassword("Vivek@1232");
+		String passwordPut = customerLogin.getPassword();
+		System.out.println("Password: "+passwordPut);
+
+		customerLogin.loginBtnClick();
+		Thread.sleep(1000);
+		System.out.println(driver.getCurrentUrl());
+		
+		String invalidError = "Invalid username or password";
+		Assert.assertEquals(driver.findElement(By.xpath("//div[@class='MuiAlert-message css-1xsto0d']")).getText(), invalidError);
+
+		String expectedUrl = "https://www.quickvee.com/customer-login";
+		Assert.assertEquals(driver.getCurrentUrl(), expectedUrl);
+		
+		}
+	
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+}
---
+++ src/test/java/testCases/customerLogin/TC_CustomerLogin_006.java
@@ -0,0 +1,62 @@
+//Verify logging into the Application using invalid email address and valid Password)
+
+package testCases.customerLogin;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import pageObjects.CustomerLogin;
+import pageObjects.HomeFrontEndPage;
+
+@Listeners(utilities.TestListener.class)
+
+public class TC_CustomerLogin_006 {
+
+WebDriver driver;
+	
+	@BeforeClass
+	public void setUp() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://www.quickvee.com/");
+	}
+	
+	@Test(priority = 1)
+	public void validEmailPass() throws InterruptedException {
+		HomeFrontEndPage homePage = new HomeFrontEndPage(driver);
+		homePage.loginBtn();
+		
+		CustomerLogin customerLogin = new CustomerLogin(driver);
+		
+		customerLogin.setEmail("vivek22@gmail.com");
+		String emailPut = customerLogin.getEmail();
+		System.out.println("Email: "+emailPut);
+		
+		customerLogin.setPassword("Vivek@123");
+		String passwordPut = customerLogin.getPassword();
+		System.out.println("Password: "+passwordPut);
+
+		customerLogin.loginBtnClick();
+		Thread.sleep(1000);
+	//	System.out.println(driver.getCurrentUrl());
+		
+		String expectedUrl = "https://www.quickvee.com/";
+		Assert.assertEquals(driver.getCurrentUrl(), expectedUrl);
+		
+		homePage.loggedIn();
+		System.out.println("Customer Successfully Logged In");
+		}
+	
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+}
